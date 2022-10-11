c38c43b123d3834aa0a455f0579a3272144e0bee
Fiorella Moreschi <gianiben17@gmail.com>
2022-10-11 16:40:49 -0300

    feat: agregando libreria al settings

--- app_recordatorio/settings.py
+++ app_recordatorio/settings.py
@@ -87,15 +87,6 @@ WSGI_APPLICATION = 'app_recordatorio.wsgi.application'
 
 
 # Database
-# Base de datos
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-#}
 
 DATABASES = {
     'default': {
